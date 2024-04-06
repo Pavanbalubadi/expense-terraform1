cc5ba66bf3f38d67cd50c22bfbb2c4824af32b46
pavan <balubadipavan@gmail.com>
2024-04-06 12:59:00 +0530

    TF code app

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -67,3 +67,80 @@ resource "aws_autoscaling_group" "main" {
 
 }
 
+resource "aws_iam_role" "main" {
+  name = "${var.env}-${var.component}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags                   = merge(var.tags, { Name = "${var.env}-${var.component}" })
+
+
+  inline_policy {
+    name = "SSM-Read-Access"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+
+          "Sid": "GetParameters",
+          "Effect": "Allow",
+          "Action": [
+            "ssm:GetParameterHistory",
+            "ssm:GetParametersByPath",
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource":[
+            "arn:aws:ssm:us-east-1:955993398443:parameter/${var.env}.${var.component}.*",
+            "arn:aws:ssm:us-east-1:955993398443:parameter/NEW_RELIC_API_KEY",
+            "arn:aws:ssm:us-east-1:955993398443:parameter/grafana_api_key",
+            "arn:aws:ssm:us-east-1:955993398443:parameter/${var.env}.rds.*",
+            "arn:aws:ssm:us-east-1:955993398443:parameter/jenkins.*"
+
+          ]
+        },
+        {
+          "Sid" : "ListResources",
+          "Effect" : "Allow",
+          "Action" : "ssm:DescribeParameters",
+          "Resource" : "*"
+        },
+        {
+          "Sid": "S3Uploadtoprometheusalerts",
+          "Effect": "Allow",
+          "Action": [
+            "s3:PutObject",
+            "s3:GetObjectAcl",
+            "s3:GetObject",
+            "s3:DeleteObjectVersion",
+            "s3:PutObjectVersionAcl",
+            "s3:GetObjectAttributes",
+            "s3:ListBucket",
+            "s3:DeleteObject",
+            "s3:PutObjectAcl"
+          ],
+          "Resource": [
+            "arn:aws:s3:::bpk-prometheus-alerts",
+            "arn:aws:s3:::bpk-prometheus-alerts/*"
+          ]
+        }
+      ]
+    })
+  }
+}
+resource "aws_iam_instance_profile" "main" {
+  name = "${var.env}-${var.component}"
+  role = aws_iam_role.main.name
+}
+
